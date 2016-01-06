97cf69f913a82aa524daf507a680a020682a2f22
root <root@ubuntu.(none)>
2016-01-06 10:58:35 -0500

    a

--- rerun.sh
+++ rerun.sh
@@ -1,13 +1,13 @@
 
 
-date && echo "start uni light uni moderate" && \
-./run_bench_Array_granular.sh myapp uni-light 10 uni-moderate && \
-date && echo "start uni medium uni moderate" && \
+#date && echo "start uni light uni moderate" && \
+#./run_bench_Array_granular.sh myapp uni-light 10 uni-moderate && \
+#date && echo "start uni medium uni moderate" && \
 ./run_bench_Array_granular.sh myapp uni-medium 10 uni-moderate && \
-date && echo "start uni heavy uni moderate" && \
-./run_bench_Array_granular.sh myapp uni-heavy 10 uni-moderate && \
-date && echo "start uni heavy uni moderate" && \
-./run_bench_Array_granular.sh myapp bimo-medium 10 uni-moderate && \
+#date && echo "start uni heavy uni moderate" && \
+#./run_bench_Array_granular.sh myapp uni-heavy 10 uni-moderate && \
+#date && echo "start uni heavy uni moderate" && \
+#./run_bench_Array_granular.sh myapp bimo-medium 10 uni-moderate && \
 
 #./run_bench_Array_granular.sh myapp bimo-medium 10 uni-longRTXen && \
 #date && echo "start bimo-medium uni moderate" && \
